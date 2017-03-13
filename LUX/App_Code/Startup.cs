2ab13c1b7b68b20cb529cfc03aa9ca40076b41fd
dieterverhellen <dieterverhellen@gmail.com>
2017-03-13 09:48:58 +0100

    Start_van_project

---
+++ LUX/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LUX.Startup))]
+namespace LUX
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
